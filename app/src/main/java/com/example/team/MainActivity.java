04dc8d26d4c8dacc8d5332d3a13beb8b62150bc1
lihaha104 <18244505922@qq.com>
2020-12-30 19:21:49 +0800

    hbuyhsdcyujgfcdhj

---
+++ app/src/main/java/com/example/team/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.team;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Toast.makeText(this, "hahahah", Toast.LENGTH_SHORT).show();
+    }
+}
