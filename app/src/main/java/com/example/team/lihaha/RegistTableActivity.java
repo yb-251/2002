3d1bdd287a5a47366b04a2c5b4b64428f257a748
lihaha104 <18244505922@qq.com>
2021-01-02 23:50:40 +0800

    李哈哈的1.2

---
+++ app/src/main/java/com/example/team/lihaha/RegistTableActivity.java
@@ -0,0 +1,16 @@
+package com.example.team.lihaha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.team.R;
+
+public class RegistTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist_table);
+    }
+}
